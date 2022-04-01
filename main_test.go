0ccb9bc84bc230342715bb63345db901fa605b97
rhc07 <rorie@ori.co>
2022-04-01 21:37:35 +0100

    set up unit tests

--- main_test.go
+++ main_test.go
@@ -7,26 +7,39 @@ import (
 )
 
 var (
-	testInput = "http://www.blankwebsite.com/"
-	//invalidInput            = "http://invalidurl.com/"
-	testResponse, testError = http.Get(testInput)
-	testBody                = testResponse.Body
+	testInput       = "http://www.blankwebsite.com/"
+	testResponse, _ = http.Get(testInput)
+	testBody        = testResponse.Body
 )
 
 func TestGetResponseBody(t *testing.T) {
 	tests := []struct {
 		input string
 		body  io.Reader
-		error error
 	}{
-		{input: testInput, body: testBody, error: nil},
-		//{input: invalidInput, body: nil, error: testError},
+		{input: testInput, body: io.Reader(testBody)},
 	}
 
 	for _, test := range tests {
 		err := GetResponseBody(test.input)
 		if err != test.body {
 			t.Errorf("Input: %s was incorrect, got: %s, want: %s", test.input, err, test.body)
+		}
+	}
+}
+
+func TestGetAnchorLinks(t *testing.T) {
+	tests := []struct {
+		input  io.Reader
+		output []string
+	}{
+		{input: testResponse.Body, output: []string{"http://www.pointlesssites.com/"}},
+	}
+
+	for _, test := range tests {
+		actual := GetAnchorLinks(testBody)
+		if actual != nil {
+			t.Errorf("Input: %s was incorrect, got: %s, want: %s", test.input, actual, test.output)
 		}
 	}
 }
